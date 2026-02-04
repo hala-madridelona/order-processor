8cf9bf3ec833083c3e3abe5765f2f2c4f3a79d7d
Hala Madrid <shas5179hank@gmail.com>
2026-02-05 00:11:22 +0530

    [FEAT] Basic SQS client setup

--- src/lib/api/orders/create-order.ts
+++ src/lib/api/orders/create-order.ts
@@ -3,6 +3,7 @@ import { Request, Response } from "express";
 import { pgClient } from "../../sql/client.js";
 import { orders } from "../../sql/models/orders.js";
 import { eq } from "drizzle-orm";
+import { OrderQueue } from "../../queue/order-queue.js";
 
 export const createOrder = async (request: Request, response: Response) => {
 
@@ -25,13 +26,22 @@ export const createOrder = async (request: Request, response: Response) => {
             return response.status(409).send('Client seems to be in an inconsistent state');
         }
 
-        const newOrder = await client.insert(orders).values({
+        const newOrderResult = await client.insert(orders).values({
             idempotencyKey: idemKey,
             userId: "sample-user"
         }).returning({
             id: orders.id,
             status: orders.status
         })
+
+        const newOrder = newOrderResult?.[0];
+        const newOrderId = newOrder?.id;
+
+        try {
+            await OrderQueue.enqueue({ orderId: newOrderId});
+        } catch (error){
+            console.log("Something went wrong with SQS");
+        }
     
         return response.status(200).send(JSON.stringify(newOrder));
 
---
+++ src/lib/queue/order-queue.ts
@@ -0,0 +1,59 @@
+import { DeleteMessageCommand, ReceiveMessageCommand, SendMessageCommand, SQSClient } from "@aws-sdk/client-sqs";
+
+const isLocal = process.env.NODE_ENV === 'local';
+
+export class OrderQueue {
+    static queue: SQSClient;
+  
+    static initQueue() {
+      if (!OrderQueue.queue) {
+        OrderQueue.queue = new SQSClient({
+          region: "ap-south-1",
+          ...(process.env.NODE_ENV === "local" && {
+            endpoint: "http://localhost:4566",
+            credentials: {
+              accessKeyId: "test",
+              secretAccessKey: "test",
+            },
+          }),
+        });
+      }
+    }
+  
+    static get client() {
+      if (!OrderQueue.queue) OrderQueue.initQueue();
+      return OrderQueue.queue;
+    }
+  
+    static enqueue(payload: unknown) {
+      return this.client.send(
+        new SendMessageCommand({
+          QueueUrl: process.env.AWS_SQS_ORDER_QUEUE_URL!,
+          MessageBody: JSON.stringify(payload),
+        })
+      );
+    }
+  
+    static async receiveOne() {
+      const res = await this.client.send(
+        new ReceiveMessageCommand({
+          QueueUrl: process.env.AWS_SQS_ORDER_QUEUE_URL!,
+          MaxNumberOfMessages: 1,
+          WaitTimeSeconds: 20,
+          VisibilityTimeout: 30,
+        })
+      );
+  
+      return res.Messages?.[0] ?? null;
+    }
+  
+    static delete(receiptHandle: string) {
+      return this.client.send(
+        new DeleteMessageCommand({
+          QueueUrl: process.env.AWS_SQS_ORDER_QUEUE_URL!,
+          ReceiptHandle: receiptHandle,
+        })
+      );
+    }
+  }
+  
